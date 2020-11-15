4323e83568bffcd2c9464d84e7def2a39703f762
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2020-11-15 11:41:28 -0500

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+
+set -e
+
+export ANSI_YELLOW="\e[1;33m"
+export ANSI_GREEN="\e[32m"
+export ANSI_RESET="\e[0m"
+
+echo -e "\n $ANSI_YELLOW *** testing docker run - redis *** $ANSI_RESET \n"
+
+echo -e "$ANSI_YELLOW Download, test connection of redis: $ANSI_RESET"
+docker run -i --rm --name some-redis quay.io/ibmz/redis:6 ping
+
+echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"

d3257d50fcca684406071c5606f927d482e6d5f5
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2020-11-15 11:57:03 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,6 +8,7 @@ export ANSI_RESET="\e[0m"
 echo -e "\n $ANSI_YELLOW *** testing docker run - redis *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW Download, test connection of redis: $ANSI_RESET"
-docker run -i --rm --name some-redis quay.io/ibmz/redis:6.0 ping
+docker run -i --rm --name some-redis quay.io/ibmz/redis:6.0 redis-cli
+docker exec -i some-redis ping
 
 echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
